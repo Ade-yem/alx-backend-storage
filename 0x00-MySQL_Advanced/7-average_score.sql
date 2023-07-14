ca44984a4a9fa0762dcd351b33d0a4fd1721d674
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-07-14 01:09:26 +0100

    up to indexing

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
+BEGIN
+    DECLARE `avg_score` FLOAT;
+    SELECT AVG(score) INTO `avg_score` FROM `corrections` WHERE `corrections`.`user_id` = `user_id`;
+    UPDATE `users` SET `average_score` = `avg_score` WHERE `users`.`id` = `user_id`;
+END //
+DELIMITER ;
